83c81fa0258edcdf0c28cdb7e912b03dfabedb13
wagnerpx <impussivo@gmail.com>
2025-04-29 09:13:25 -0300

    Iniciando no JS

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+/*
+
+OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o próximo 
+cartão da lista
+  - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
+  - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+  - passo 3 - fazer aparecer o próximo cartão da lista 
+  - passo 4 - buscar o cartão que está selecionado e esconder
+
+OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
+anterior da lista
+  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+  - passo 3 - fazer aparecer o cartão anterior da lista
+  - passo 4 - buscar o cartão que está selecionado e esconder
+*/
+
+// - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
+const btnAvancar = document.getElementById("btn-avancar");
+
+// - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+btnAvancar.addEventListener("click", function () {
+    // - passo 3 - fazer aparecer o próximo cartão da lista
+    
+});
