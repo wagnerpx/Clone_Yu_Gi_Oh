617601d1e59951e13835c1df107e48bbd07e5600
Wagner Alexandre <impussivo@gmail.com>
2025-04-29 15:55:17 -0300

    Refatorando JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,32 @@
-/*
 
-OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o próximo 
-cartão da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
-  - passo 3 - fazer aparecer o próximo cartão da lista 
-  - passo 4 - buscar o cartão que está selecionado e esconder
-
-OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
-anterior da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
-  - passo 3 - fazer aparecer o cartão anterior da lista
-  - passo 4 - buscar o cartão que está selecionado e esconder
-*/
-
-// - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
 btnAvancar.addEventListener("click", function () {
-    if(cartaoAtual === cartoes.length -1) return;
-    console.log(cartoes.length);
-    
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+  if (cartaoAtual === cartoes.length - 1) return;
 
-    cartaoAtual++;
-    cartoes[cartaoAtual].classList.add("selecionado");
-    
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao();
 });
-
 
 btnVoltar.addEventListener("click", function () {
-    if(cartaoAtual === 0 ) return;
-    
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+  if (cartaoAtual === 0) return;
 
-    cartaoAtual--;
-    cartoes[cartaoAtual].classList.add("selecionado");
-    
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao();
 });
+
+function mostrarCartao() {
+  cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
