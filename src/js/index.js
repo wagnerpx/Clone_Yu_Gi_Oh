b73a7d978db48b5331a65bcab54cb186c96e71d9
Wagner Alexandre <impussivo@gmail.com>
2025-04-30 16:14:25 -0300

    Adicionando animação a carta

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,20 @@
-
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
+
+cartoes.forEach(cartao => {
+  cartao.addEventListener("click", function () {
+    const cartaVirada = cartao.querySelector(".carta-virada");
+
+    cartao.classList.toggle("virar");
+    
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+    
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  });
+});
 
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return;
